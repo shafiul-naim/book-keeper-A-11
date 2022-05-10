c128610d34df53423e1833b2763ac241e23a7c08
shafiul-naim <shafiul.naim@gmail.com>
2022-05-10 11:15:37 +0600

    inventory added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Inventories from '../Inventories/Inventories';
+
+const Home = () => {
+    return (
+        <div>
+            <Inventories></Inventories>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/Inventories/Inventories.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import Inventory from '../Inventory/Inventory';
+
+const Inventories = () => {
+    const [inventories, setInventories] = useState([]);
+
+    useEffect(() => {
+        fetch('inventories.json')
+        .then(res => res.json())
+        .then(data => setInventories(data))
+    }, ([]))
+    return (
+        <div id="services" className="container mt-5 ">
+        <h2 className="text-center mt-5 text-primary ">My Services</h2>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+      {inventories.map((service) => (
+        <Inventory key={service.id} service={service}></Inventory>
+      ))}
+      </div>
+    </div>
+    );
+};
+
+export default Inventories;
