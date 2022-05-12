21f4925c25fb7dc3a1cfe3fcba3f65c23a109228
shafiul-naim <shafiul.naim@gmail.com>
2022-05-12 21:10:01 +0600

    some data modification

--- src/Pages/Home/Inventories/Inventories.js
+++ src/Pages/Home/Inventories/Inventories.js
@@ -11,7 +11,7 @@ const Inventories = () => {
     }, ([]))
     return (
         <div id="inventory" className="container mt-5 ">
-        <h2 className="text-center mt-5 text-primary ">My Services</h2>
+        <h2 className="text-center my-5 text-primary ">Book Inventory</h2>
       <div className="row row-cols-1 row-cols-md-3 g-4 border-1">
       {inventories.map((inventory) => (
         <Inventory key={inventory.id} inventory={inventory}></Inventory>
