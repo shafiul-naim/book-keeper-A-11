df72e2ca05292dda7b52ed1d162251a88dbc40b8
shafiul-naim <shafiul.naim@gmail.com>
2022-05-12 12:05:21 +0600

    firebase.init.js file hidden

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAun8CXtWrUM8oa5a7N5YYAYyww52H5Adw",
-  authDomain: "warehouse-management-a-11.firebaseapp.com",
-  projectId: "warehouse-management-a-11",
-  storageBucket: "warehouse-management-a-11.appspot.com",
-  messagingSenderId: "590143988595",
-  appId: "1:590143988595:web:9137617bfe20f0dc4e9b06"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
