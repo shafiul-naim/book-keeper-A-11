64ef77a26e2848f8a04e69004a1606d0db490ca2
shafiul-naim <shafiul.naim@gmail.com>
2022-05-15 22:40:43 +0600

    avaiable books

--- src/Pages/Books/Books.js
+++ src/Pages/Books/Books.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { Button, Card, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card, ListGroup, Modal, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Books = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="container my-5">
       <Card>
@@ -12,8 +16,8 @@ const Books = () => {
               <Nav.Link href="#first">Old book</Nav.Link>
             </Nav.Item>
             <Nav.Link as={Link} to="available">
-                  All books
-                </Nav.Link>
+              All books
+            </Nav.Link>
           </Nav>
         </Card.Header>
         <Card.Body>
@@ -22,7 +26,34 @@ const Books = () => {
             Some of the old books are available in our inventory.You can buy
             these at our warehouse.
           </Card.Text>
-          <Button variant="primary">Book list</Button>
+          <Button variant="primary" onClick={handleShow}>
+            Book list
+          </Button>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title> Old book list</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <ListGroup>
+                <ListGroup.Item>Inside the Machine</ListGroup.Item>
+                <ListGroup.Item>Structure and Interpretation of Computer Programs</ListGroup.Item>
+                <ListGroup.Item>Code: The Hidden Language of Computer Hardware and Software</ListGroup.Item>
+                <ListGroup.Item>Cracking the Coding Interview: 189 Programming Questions and Solutions</ListGroup.Item>
+                <ListGroup.Item>Programming Pearls</ListGroup.Item>
+              </ListGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </Card.Body>
       </Card>
     </div>
